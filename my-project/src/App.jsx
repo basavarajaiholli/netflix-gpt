0d65d49ef3b1f74654967a8b5b6ab738c4c98815
basavarajaiholli <basavarajaiholli280@gmail.com>
2024-10-23 12:37:55 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold text-green-800  underline">
+      lets Build Netflix-GPT
+    </h1>
+  );
+};
+export default App;
